49ab2f14c8f19304f4abc154146438182273f6d5
raymond <raymondfru412@gmail.com>
2024-11-30 12:36:27 +0100

    implement web hooks

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,7 @@
-import {clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/api/webhooksclerk", "/services(.*)"]); //addYourSpecificRoutesInHereInTheFormOfAnArrayElement
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    (await auth()).userId;
-  }
-});
-
+export default clerkMiddleware();
+ 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
